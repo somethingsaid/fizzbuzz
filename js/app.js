86e5c084aefaac163f88cf0180c34ee2ec3fd8c0
somethingsaid <kwong.billy@gmail.com>
2016-03-01 11:56:56 -0500

    Refactored by adding user prompts

--- js/app.js
+++ js/app.js
@@ -1,37 +1,35 @@
+
+// Initializing global variables
 var output = '';
+var userInput = parseInt(prompt("Please enter an integer greater than 0; decimals will be dropped.\n\rExample 1 - 597a will be read as 597.\nExample 2 - 12.9 will be read as 12."), 10);
+console.log("user input resolved to: " + userInput);
+console.log(typeof(userInput));
 
-
-for (var i = 1; i <= 100; i++) {
-	if (i % 3 == 0) {
-		output = output + 'Fizz';
-	};
-	if (i % 5 == 0) {
-		output = output + 'Buzz';
-	};
-	if (output.length == 0) {
-		output = i.toString();
-	}
-	document.write('<p>' + output + '</p>');
-    console.log(output);
-    output = '';
+// fizzBuzz function
+function fizzBuzz(n) {
+    for (var i = 1; i <= n; i++) {
+	    if (i % 3 == 0) {
+		    output = output + 'Fizz';
+	    };
+	    if (i % 5 == 0) {
+		    output = output + 'Buzz';
+	    };
+	    if (output.length == 0) {
+		    output = i.toString();
+	    }
+	    document.write('<p>' + output + '</p>');
+        console.log(output);
+        output = '';
+    }
 }
 
-
-/* this took a couple of extra lines
-for (var i = 1; i <= 100; i++) {
-	if ((i % 3 == 0) && (i % 5 == 0)) {
-		output = 'FizzBuzz';
-	}
-	else if (i % 3 == 0) {
-		output = 'Fizz';
-	}
-	else if (i % 5 == 0) {
-		output = 'Buzz';
-	}
-    else {
-    	output = i.toString();
-    };
-    document.write('<p>' + i + ': ' + output + '</p>');
-    console.log(output);
-};
-*/
+// only run fizzBuzz if userInput is a number > 0
+if (isNaN(userInput)) {
+	alert("Invalid input, please reload the page and try again with a positive integer.");
+}
+else if (userInput <= 0) {
+	alert("Please reload the page and try again with a positive integer");
+}
+else {
+	fizzBuzz(userInput);
+}
